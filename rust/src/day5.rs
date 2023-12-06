4fe731f780524ac53167c44d5edece2f44f13b98
gugomea <gugomea@unirioja.es>
2023-12-06 18:35:10 +0100

    small cleanup

--- rust/src/day5.rs
+++ rust/src/day5.rs
@@ -5,36 +5,36 @@ pub fn solve() {
     part2();
 }
 
-fn part2() {
-    //let file = fs::read_to_string("../input/test.txt").unwrap();
+fn get_seed_maps() -> (Vec<usize>, Vec<Vec<(usize, usize, usize)>>) {
     let file = fs::read_to_string("../input/day5.txt").unwrap();
     let mut file = file.split("\n\n");
     let seeds = match file.next() {
         Some(seeds) => seeds.split(" ").filter_map(|x| x.parse::<usize>().ok()),
         None => panic!("No seeds provided"),
-    }.collect::<Vec<_>>();
+    }.collect();
 
     let maps = file.map(parse_map).map(|mut v|{
         v.sort_by_key(|(_, b, _)| *b);
         v
-    }).collect::<Vec<_>>();
+    }).collect();
 
-    //let aux = maps.clone()
-    //    .into_iter()
-    //    .for_each(|v|{
-    //        let v = v.into_iter()
-    //         .flat_map(|(_a, b,c)| vec![b, b + c - 1])
-    //         .collect::<Vec<_>>();
-    //        let (mut i, mut j) = (1, 2);
-    //        for _ in 0..((v.len() / 2) - 1) {
-    //            let pred = v[i] +1 == v[j];
-    //            println!("{}", pred);
-    //            i += 2;
-    //            j += 2;
-    //        }
-    //    });
+    (seeds, maps)
+}
 
-    println!("empezamos");
+fn part1() {
+    let (seeds, maps) = get_seed_maps();
+
+    let mut min = usize::MAX;
+    for &seed in seeds.iter() {
+        let aux = calculate_range_seed((seed, seed), &maps, 0);
+        if aux < min { min = aux; }
+    }
+    println!("{}", min);
+}
+
+fn part2() {
+    let (seeds, maps) = get_seed_maps();
+
     let mut min = usize::MAX;
     for slice in seeds.chunks(2) {
         if let &[start, size] = slice {
@@ -43,25 +43,20 @@ fn part2() {
         }
     }
     println!("{}", min);
-    //println!("{:?}", seeds.chunks(2).collect::<Vec<_>>());
 }
 
 fn calculate_range_seed((start, end): (usize, usize), remaining_maps: &[Vec<(usize, usize, usize)>], i: usize) -> usize {
     let next = remaining_maps.get(i);
 
     if let Some(map) = next {
-        //println!("Dado el intervalo ({}, {}) y el mapa {:?}", start, end, map);
         let intervals = partition_intervals(start, end, map);
-        //println!(">> {:?}", intervals);
         let mut min = usize::MAX;
         for &interval in intervals.iter() {
             let possible = calculate_range_seed(interval, remaining_maps, i + 1);
-            if possible == 0 { println!("hola"); }
             if possible < min { min = possible; }
         }
         return min;
     }
-
     return start;
 }
 
@@ -72,21 +67,21 @@ fn partition_intervals(start: usize, end: usize, ranges: &Vec<(usize, usize, usi
         if i < ranges.len() -1 {
             let this_end = ranges[i].1 + ranges[i].2 - 1;
             let next_start = ranges[i+1].1;
+            //there is a whole between intervals
             if this_end + 1 != next_start {
                 let this_end = usize::max(start, this_end + 1);
                 let this_end_end = usize::min(end, next_start - 1);
                 result.push((this_end, this_end_end));
             }
         }
+
         let range = ranges[i];
         let (mut s, mut e) = (range.1, range.1 + range.2 - 1);
 
         if e < start { continue; }
         s = usize::max(s, s_current);
-
         if s > end { continue; }
         e = usize::min(e, end);
-
 
         let long = e - s + 1;
         let diff = s as i64 - range.1 as i64;
@@ -96,43 +91,14 @@ fn partition_intervals(start: usize, end: usize, ranges: &Vec<(usize, usize, usi
         if i < ranges.len() - 1 { s_current = ranges[i + 1].1; }
     }
 
-    let mut first_padd = None;
-    let mut last_padd = None;
-
-    if start < ranges[0].1 {
-        first_padd = Some((start, usize::min(end, ranges[0].1 - 1)));
-    }
+    if start < ranges[0].1 { result.push((start, usize::min(end, ranges[0].1 - 1))); }
 
     let last = ranges.last().unwrap();
+    if end > (last.1 + last.2 - 1) { result.push((usize::max(start, last.1 + last.2), end)); }
 
-    if end > (last.1 + last.2 - 1) {
-        last_padd = Some((usize::max(start, last.1 + last.2), end));
-    }
-
-    if let Some(first_padd) = first_padd { result.push(first_padd); }
-    if let Some(last_padd) = last_padd { result.push(last_padd); }
-    //if first_padd != last_padd { result.push(last_padd); }
     let result = result.into_iter().filter(|(s, e)| s <= e).collect();
-    //println!(">> {:?}", result);
 
     return result;
-}
-
-fn part1() {
-    let file = fs::read_to_string("../input/day5.txt").unwrap();
-    let mut file = file.split("\n\n");
-    let seeds = match file.next() {
-        Some(seeds) => seeds.split(" ").filter_map(|x| x.parse::<usize>().ok()),
-        None => panic!("No seeds provided"),
-    }.collect::<Vec<_>>();
-    let maps = file.map(parse_map).collect::<Vec<_>>();
-
-    let mut min = usize::MAX;
-    for seed in seeds.iter() {
-        let aux = calculate_seed(*seed, &maps, 0);
-        if aux < min { min = aux; }
-    }
-    println!("{}", min);
 }
 
 fn parse_map(s: &str) -> Vec<(usize, usize, usize)> {
@@ -144,22 +110,4 @@ fn parse_map(s: &str) -> Vec<(usize, usize, usize)> {
                 _ => panic!("not valid seed map"),
             }
     }).collect()
-}
-
-fn calculate_seed(seed: usize, remaining_maps: &[Vec<(usize, usize, usize)>], i: usize) -> usize {
-    let next = remaining_maps.get(i);
-
-    if let Some(map) = next {
-        for sentence in map {
-            let (a, b, c) = *sentence;
-            let valid = seed >= b && seed <= (b + c);
-            if  valid { 
-                let next = ( seed - b ) + a;
-                return calculate_seed(next, remaining_maps, i + 1);
-            }
-        }
-        return calculate_seed(seed, remaining_maps, i + 1);
-    }
-
-    return seed;
 }
