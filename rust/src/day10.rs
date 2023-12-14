3f0bf109dd232027ba44ed5d0f9002ad39d4d9ca
guillermo <gugomea@unirioja.es>
2023-12-14 22:41:58 +0100

    it works, but please, don't look at the code until saturday at least

--- rust/src/day10.rs
+++ rust/src/day10.rs
@@ -45,18 +45,59 @@ fn dfs(i: i64, j: i64, v: &mut Vec<Vec<usize>>) {
     dfs(i - 1, j, v);
     dfs(i, j + 1, v);
     dfs(i, j - 1, v);
+}
 
-    dfs(i + 1, j - 1, v);
-    dfs(i + 1, j + 1, v);
+fn follow_path(matrix: &mut Vec<Vec<[(i64, i64); 2]>>, grid: &mut Vec<Vec<usize>>, i: i64, j: i64, value: usize, flag: bool) {
+    let [d1, d2] = matrix[i as usize][j as usize];
+    let [aux_d1, aux_d2] = matrix[i as usize][j as usize];
+    let [d1, d2] = [(i + d1.0, j + d1.1), (i + d2.0, j + d2.1)];
+    let expected = value + 1;
+    //println!("value: {}", value);
+    if grid[d1.0 as usize][d1.1 as usize] == expected {
 
-    dfs(i - 1, j - 1, v);
-    dfs(i - 1, j + 1, v);
+        let mut direccion = (d1.1 - j, d1.0 - i);
+        if direccion.1 == 0 { direccion.0 *= -1; }
 
+        if flag {
+            direccion.0 *= -1;
+            direccion.1 *= -1;
+        }
+
+        let (i, j) = (i + direccion.0, j + direccion.1);
+        //println!("estando en {}, me meto a ({}, {})", value, i, j);
+        dfs(i, j, grid);
+        let (i, j) = (d1.0 + direccion.0, d1.1 + direccion.1);
+        dfs(i, j, grid);
+        //for r in grid.iter() {
+            //println!("{:?}", r);
+        //}
+        follow_path(matrix, grid, d1.0, d1.1, expected, flag);
+    } else if grid[d2.0 as usize][d2.1 as usize] == expected {
+        let mut direccion = (d2.1 - j, d2.0 - i);
+        if direccion.1 == 0 { direccion.0 *= -1; }
+
+        if flag {
+            direccion.0 *= -1;
+            direccion.1 *= -1;
+        }
+
+        let (i, j) = (i + direccion.0, j + direccion.1);
+        //println!("estando en {}, me meto a ({}, {})", value, i, j);
+        dfs(i, j, grid);
+        let (i, j) = (d2.0 + direccion.0, d2.1 + direccion.1);
+        dfs(i, j, grid);
+        //for r in grid.iter() {
+            //println!("{:?}", r);
+        //}
+        follow_path(matrix, grid, d2.0, d2.1, expected, flag);
+
+    } else {}
 }
 
 fn part1() {
 
-    let file = fs::read_to_string("../input/test.txt").unwrap();
+    let file = fs::read_to_string("../input/day10.txt").unwrap();
+    //let file = fs::read_to_string("../input/test.txt").unwrap();
     let (mut s_i, mut s_j) = (0, 0);
     let mut grid = file.lines().enumerate().map(|(i, line)| {
         line.chars().enumerate().map(|(j, ch)| {
@@ -78,27 +119,47 @@ fn part1() {
     let mut visited = vec![vec![false; m]; n];
     let mut matrix = vec![vec![0; m]; n];
 
-
-    //let initial = [(1, 0), (0, 1)];
-    //let given = [(-1, 0), (0, -1)];
-
     let initial = [(1, 0), (0, 1)];
     let given = [(-1, 0), (0, -1)];
+
+    //let initial = [(1, 0), (0, -1)];
+    //let given = [(-1, 0), (0, 1)];
     grid[s_i as usize][s_j as usize] = initial;
     bfs(given, s_i, s_j, &mut matrix, &mut grid, &mut visited);
-    for i in 0..n {
-        for j in 0..m {
-            //if i == 0 || j == 0 || i == n - 1 || j == m - 1 { dfs(i as i64, j as i64, &mut matrix); }
-        }
-    }
 
     for row in matrix.clone() {
         println!("{:?}", row);
     }
 
+    println!("===========================");
+    println!("===========================");
+
+    follow_path(&mut grid, &mut matrix, s_i + initial[0].0, s_j + initial[0].1, 1, false);
+    follow_path(&mut grid, &mut matrix, s_i + initial[1].0, s_j + initial[1].1, 1, true);
+
+    matrix[s_i as usize][s_j as usize] = 0;
+
+    for (i, row) in matrix.clone().into_iter().enumerate() {
+        for (j, element) in row.clone().into_iter().enumerate() {
+            if i == s_i as usize && j == s_j as usize {
+                print!("S");
+                continue;
+            }
+            if element == 0 {
+                print!(".");
+            } else if element != usize::MAX {
+                let p: char = '\u{2588}';
+                print!("{}", p);
+            } else { 
+                print!("I");
+            }
+        }
+        println!();
+    }
+
     println!("{}", matrix.into_iter().map(|line| line
             .into_iter()
-            .filter(|&x| x == 0)
+            .filter(|&x| x == usize::MAX)
             .count())
         .sum::<usize>());
 
